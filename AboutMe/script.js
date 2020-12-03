6b572955a964ce7c8a1d59fa280167e515b4b25b
Sergio W. Peterson <Sergiopeter2016@berkeley.edu>
2020-12-02 23:14:00 -0800

    Starting Gallery

---
+++ AboutMe/script.js
@@ -0,0 +1,13 @@
+const Gallery_rightButton = document.querySelector('.Gallery_Right');
+const Gallery_leftButton = document.querySelector('.Gallery_Left');
+const Gallery_1 = document.querySelector('.Gallery_1');
+const Gallery1_Images = [
+  '../Img/holder2.jpg',
+  '../Img/holder.jpg'
+]
+var gallery1_index = 0;
+
+Gallery_rightButton.addEventListener("click", function(){
+  if 
+});
+
