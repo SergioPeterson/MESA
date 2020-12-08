e0b83e0a22dec8abf4113698a51f5422727293c2
Sergio W. Peterson <Sergiopeter2016@berkeley.edu>
2020-12-08 14:29:37 -0800

    Maked Gallery for MIt

---
+++ MITPage/script.js
@@ -0,0 +1,32 @@
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const image = document.querySelector('.gallery_image');
+const gallery_images = [
+    '../Img/MITImage1.jpg',
+    '../Img/MITImage2.jpg',
+    '../Img/MITImage3.jpg',
+    '../Img/MITHacks.jpg',
+    '../Img/MITImage5.jpg'
+]
+var pos = 0
+const start_scroll = '2484px';
+
+image.src = gallery_images[pos];
+function nextImg(){
+    if(pos == gallery_images.length-1){
+        pos = 0
+        image.src = gallery_images[pos]
+    }else{
+        pos = pos + 1
+        image.src = gallery_images[pos]
+    }
+}
+function prevImg(){
+    if(pos == 0){
+        pos = gallery_images.length - 1
+        image.src = gallery_images[pos]
+    }else{
+        pos = pos - 1
+        image.src = gallery_images[pos]
+    }
+}
